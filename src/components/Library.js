402b121adcd72e9738faebda23c5ffd3c7e0a4a5
Adigboluja Ganiyat <devghandy@gmail.com>
2022-03-08 15:54:47 -0800

    second commit

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,4 +1,4 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
+// import { library } from '@fortawesome/fontawesome-svg-core';
 import React from 'react';
 import LibrarySong from './LibrarySong';
 
